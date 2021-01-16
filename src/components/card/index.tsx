2c028ed974b733eb80afecb7dbf880bc7df89999
Henrique Jensen <henriquejensen@hotmail.com>
2021-01-16 20:39:13 -0300

    adds produtos list

---
+++ src/components/card/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 250,
+    margin: 10,
+  },
+});
+
+export default function ImgMediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://i.ytimg.com/vi/eSXu5BZSBHo/maxresdefault.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Trico
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            R$22.50
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Ver
+        </Button>
+        <Button size="small" color="primary">
+          Comprar
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/pages/produtos/index.tsx
+++ src/pages/produtos/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Card from "../../components/card";
 
 function Produtos() {
-  return <section>Produtos</section>;
+  const produtos = [1, 2, 3, 4, 5];
+  const classes = useStyles();
+
+  return (
+    <section className={classes.container}>
+      {produtos.map((produto) => (
+        <Card key={produto} />
+      ))}
+    </section>
+  );
 }
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+}));
 
 export default Produtos;
