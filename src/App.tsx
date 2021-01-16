e415c2355b4b603823488da2b09516cc5c23e0bd
Henrique Jensen <henriquejensen@hotmail.com>
2021-01-16 20:06:02 -0300

    adds header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Container } from "@material-ui/core";
 
 import Router from "./router";
+import Header from "./components/header";
 
 function App() {
   return (
     <Container>
+      <Header />
       <Router />
     </Container>
   );
