918a9d79af7c0ea9012f7483c15ba7e7d25bb391
Henrique Jensen <henriquejensen@hotmail.com>
2021-01-16 21:35:34 -0300

    adds produto details

--- src/pages/produto/index.tsx
+++ src/pages/produto/index.tsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-function Produto() {
-  return <section>Produto</section>;
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function OutlinedCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <CardMedia
+          component="img"
+          alt="Produto"
+          width="140"
+          height="140"
+          image="https://i.ytimg.com/vi/eSXu5BZSBHo/maxresdefault.jpg"
+          title="Produto"
+        />
+        <Typography className={classes.pos} color="textSecondary">
+          Detalhes
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Contatar Vendedor
+        </Typography>
+        <Typography variant="body2" component="p">
+          R$ 22.50
+        </Typography>
+        <CardActions>
+          <Button size="small" color="primary">
+            Acionar ao carrinho
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
 }
-
-export default Produto;
