1e96d5779e8faef22290f4a0fd33911107143985
Henrique Jensen <henriquejensen@hotmail.com>
2021-01-17 01:24:26 -0300

    congratulations screen

--- src/pages/congratulations/index.tsx
+++ src/pages/congratulations/index.tsx
@@ -1,28 +1,50 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import CardMedia from "@material-ui/core/CardMedia";
 import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
 
 import Button from "../../components/button";
 
+const useStyles = makeStyles({
+  grid: {
+    marginTop: 30,
+    marginBottom: 60,
+  },
+  message: {
+    marginTop: 30,
+  },
+});
+
 function Congratulations() {
+  const classes = useStyles();
+
   return (
     <section>
-      <CardMedia
-        component="img"
-        alt="Produto"
-        height="100"
-        width="100"
-        image="https://i.ytimg.com/vi/eSXu5BZSBHo/maxresdefault.jpg"
-        title="Produto"
-      />
-      <Typography variant="h3" component="h2">
-        Compra Efetuada!
-      </Typography>
-      <Typography variant="h5" component="h2">
-        Obrigado por contribuir com essa comunidade, uma porcentagem dessa venda
-        será doada!
-      </Typography>
+      <Grid container spacing={3} className={classes.grid}>
+        <Grid item xs={4}>
+          <CardMedia
+            component="img"
+            alt="Produto"
+            height="100"
+            width="100"
+            image="https://i.ytimg.com/vi/eSXu5BZSBHo/maxresdefault.jpg"
+            title="Produto"
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <div>
+            <Typography variant="h5" component="h1">
+              Compra Efetuada!
+            </Typography>
+            <div className={classes.message}>
+              Obrigado por contribuir com essa comunidade, uma porcentagem dessa
+              venda será doada!
+            </div>
+          </div>
+        </Grid>
+      </Grid>
       <Link to="/">
         <Button title="Continuar comprando" />
       </Link>
